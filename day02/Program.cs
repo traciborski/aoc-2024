211ea912273d7eabefe401b60af8a1d046910153
Tomasz Raciborski <tomasz.raciborski@gmail.com>
2024-12-03 23:23:15 +0100

    d2p2

--- day02/Program.cs
+++ day02/Program.cs
@@ -1,31 +1,63 @@
 ﻿
 var lines = File.ReadAllLines("input");
 
-var safe = lines.Select(x => x.Split(" ").Select(y => int.Parse(y)).ToArray()).Where(x => Safe(x)).Count();
+var safe = lines.Select(x => x.Split(" ").Select(y => int.Parse(y)).ToArray()).Where(x => Safe(x, 1)).Count();
 
-bool Safe(int[] xs)
+bool Safe(int[] xs, int margin)
 {
     sbyte dir = 0;
+
     for (int i = 1; i < xs.Length; i++)
     {
         var diff = Math.Abs(xs[i] - xs[i - 1]);
         if (diff < 1 || diff > 3)
-            return false;
+        {
+            margin--;
+            if (margin < 0)
+            {
+                return false;
+            }
+            else
+            {
+                i++;
+                continue;
+            }
+        }
 
         if (xs[i] > xs[i - 1]) // increasing
         {
             if (dir < 0)
-                return false;
+            {
+                margin--;
+                i++;
+                if (margin < 0)
+                    return false;
+                else
+                {
+                    i++;
+                    continue;
+                }
+            }
             dir = 1;
         }
         else if (xs[i] < xs[i - 1])
         {
             if (dir > 0)
-                return false;
+            {
+                margin--;
+                i++;
+                if (margin < 0)
+                    return false;
+                else
+                {
+                    i++;
+                    continue;
+                }
+            }
             dir = -1;
         }
     }
     return true;
 }
 
-Console.WriteLine(safe); // 799 too high
+Console.WriteLine(safe); 
